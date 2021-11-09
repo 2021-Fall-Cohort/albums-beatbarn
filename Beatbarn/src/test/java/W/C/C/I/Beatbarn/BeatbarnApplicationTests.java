106049b29dfc6d3056ce9cb1ad453a35d8d02bfc
Alithecoder1 <90347681+Alithecoder1@users.noreply.github.com>
2021-11-09 12:17:24 -0600

    Added spring dependencies

---
+++ Beatbarn/src/test/java/W/C/C/I/Beatbarn/BeatbarnApplicationTests.java
@@ -0,0 +1,13 @@
+package W.C.C.I.Beatbarn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatbarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
