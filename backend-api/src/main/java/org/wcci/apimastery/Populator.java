07eceb4eaaeae1a67e47f35fa085b6dedec54159
ThomasC119 <90348324+ThomasC119@users.noreply.github.com>
2021-11-10 11:14:58 -0500

    f-ing with flexboxes
    fix this

--- backend-api/src/main/java/org/wcci/apimastery/Populator.java
+++ backend-api/src/main/java/org/wcci/apimastery/Populator.java
@@ -5,29 +5,26 @@ import org.springframework.stereotype.Component;
 import org.wcci.apimastery.models.Album;
 import org.wcci.apimastery.models.Song;
 import org.wcci.apimastery.repository.AlbumRepository;
-import org.wcci.apimastery.repository.CommentRepository;
 import org.wcci.apimastery.repository.SongRepository;
 
 @Component
 public class Populator implements CommandLineRunner {
     private AlbumRepository albumRepo;
-    private CommentRepository commentRepo;
     private SongRepository songRepo;
 
-    public Populator(AlbumRepository albumRepo, CommentRepository commentRepo, SongRepository songRepo) {
+    public Populator(AlbumRepository albumRepo, SongRepository songRepo) {
         this.albumRepo = albumRepo;
-        this.commentRepo = commentRepo;
         this.songRepo = songRepo;
     }
     @Override
     public void run(String... args) throws Exception{
-
+        Album album1 = new Album("Blue Record","",5f,"Relapse Records","Baroness");
+        albumRepo.save(album1);
         Song song1 = new Song("Swollen and Halo","",6.35f,5f,album1,"Baroness");
         songRepo.save(song1);
         Song song2 = new Song("A Horse Called Golgotha","",5.21f,4f,album1,"Baroness");
         songRepo.save(song2);
-        Album album1 = new Album("Blue Record","",5f,"Relapse Records","Baroness", song1, song2);
-        albumRepo.save(album1);
+
 
     }
 }
