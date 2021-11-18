aeda288afdb00e1e7e58e70c9b51753dcf472e46
carsonDentinger <carson.dentinger@gmail.com>
2021-11-17 22:39:41 -0500

    as done as its gonna get

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,12 +8,16 @@ function displayHeader(mainContainerEl, albumsJson){
     const headerEl = document.createElement("header");
     const headerUlEl = document.createElement("ul");
     headerUlEl.classList.add("header");
-        const homeLiEl = document.createElement("li");
-        homeLiEl.innerText = "Home";
+        const homeLiEl = document.createElement("img");
+        homeLiEl.src = "/resources/barn.png";
+        homeLiEl.classList.add("homeImg");
         const missionLiEl = document.createElement("li");
         missionLiEl.innerText = "Mission Statement";
+        missionLiEl.classList.add("headerLi");
         const meetLiEl = document.createElement("li");
         meetLiEl.innerText = "Meet the Farmers";
+        meetLiEl.classList.add("headerLi");
+        meetLiEl.setAttribute('id', 'meetLink');
 
         headerUlEl.append(homeLiEl);
         headerUlEl.append(missionLiEl);
@@ -25,6 +29,80 @@ function displayHeader(mainContainerEl, albumsJson){
         clearChildren(mainContainerEl);
         fetchAlbums();
     })
+
+    // const meetModalDiv = document.createElement("div");
+    // meetModalDiv.classList.add("meetModal");
+    // meetModalDiv.setAttribute('id', 'meetModal');
+    // const meetModalContentDiv = document.createElement("div");
+    // meetModalContentDiv.classList.add("modalContentDiv");
+    // const meetModalHeaderDiv = document.createElement("div");
+    // meetModalHeaderDiv.classList.add("modalHeaderDiv");
+    // const meetModalClose = document.createElement("span");
+    // meetModalClose.classList.add("close");
+    // meetModalClose.innerHTML = "&times;";
+    // const meetModalHeader = document.createElement("h2");
+    // meetModalHeader.classList.add("modalHeader");
+    // meetModalHeader.innerText = "Thank your Farmers!";
+    // const meetModalBodyDiv = document.createElement("div");
+    // meetModalBodyDiv.classList.add("modalBodyDiv");
+
+    // const portfolioUl = document.createElement("ul");
+    // portfolioUl.classList.add("portfolioUl");
+    // const tommyLi = document.createElement("li");
+    // tommyLi.classList.add("portfolioLi");
+    // const carsonLi = document.createElement("li");
+    // carsonLi.classList.add("portfolioLi");
+    // const aliLi = document.createElement("li");
+    // aliLi.classList.add("portfolioLi");
+    // const garadLi = document.createElement("li");
+    // garadLi.classList.add("portfolioLi");    
+    // const tommyImg = document.createElement("img");
+    // tommyImg.classList.add("portfolioImg");
+    // tommyImg.src = "/resources/tommy.jpg";
+    // const carsonImg = document.createElement("img");
+    // carsonImg.classList.add("portfolioImg");
+    // carsonImg.src = "/resources/carson.jpg";
+    // const aliImg =  document.createElement("img"); 
+    // aliImg.classList.add("portfolioImg");
+    // aliImg.src = "/resources/avi.png";
+    // const garadImg  = document.createElement("img");
+    // garadImg.classList.add("portfolioImg");    
+
+
+    
+    //     tommyLi.append(tommyImg);
+    //     portfolioUl.append(tommyLi);
+    //     portfolioUl.append(carsonLi);
+    //     portfolioUl.append(aliLi);
+    //     portfolioUl.append(garadLi);
+    //     meetModalHeaderDiv.append(meetModalHeader);
+    //     meetModalDiv.append(meetModalClose);
+    //     meetModalDiv.append(meetModalHeaderDiv);
+    //     meetModalContentDiv.append(meetModalBodyDiv);
+        
+    //     meetModalDiv.append(meetModalContentDiv);
+    //     mainContainerEl.append(meetModalDiv);
+
+
+    // var modal = document.getElementById("meetModal");
+
+    // var btn = document.getElementById("meetLink");
+
+    // var span = document.getElementsByClassName("close")[0];
+
+    // btn.onclick = function() {
+    // modal.style.display = "block";
+    // }
+
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
+
+    // window.onclick = function(event) {
+    // if (event.target == modal) {
+    //     modal.style.display = "none";
+    // }
+    // }
 };
 
 fetchAlbums();
